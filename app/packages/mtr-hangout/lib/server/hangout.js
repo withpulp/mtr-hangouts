fb15228dc05556cc7b00e697e1f56b70cf4d8c42
Damir <damir@withpulp.com>
2015-09-12 13:12:38 -0400

    wip hangout.js

--- app/packages/mtr-hangout/lib/server/hangout.js
+++ app/packages/mtr-hangout/lib/server/hangout.js
@@ -1,9 +1,26 @@
-// Hangout Methods
+Meteor.methods({hangout: function (err, value) {
+    this.unblock();
+    if (err) {
+        console.log(err);
+    } else {
+        try {
+            var result = HTTP.call('GET', 'https://apis.google.com/js/platform.js');
+            return result;
+        } catch (err) {
+            // Got a network error, time-out or HTTP error in the 400 or 500 range.
+            return false;
+        }
+    }
+}});
 
-var Hangout = {};
+var Hangout = Meteor.call('hangout');
+
+
+// Hangout Methods
 
 Meteor.methods({
     renderSimpleHangout: function () {
         console.log('render simple hangout');
+        console.log(Hangout);
     }
 });

f830f983b4eff0c4c05a04d804c4ffe03bf37023
Damir <damir@withpulp.com>
2015-09-12 15:13:36 -0400

    working on render simple hangout method

--- app/packages/mtr-hangout/lib/server/hangout.js
+++ app/packages/mtr-hangout/lib/server/hangout.js
@@ -1,26 +1,16 @@
-Meteor.methods({hangout: function (err, value) {
-    this.unblock();
-    if (err) {
-        console.log(err);
-    } else {
-        try {
-            var result = HTTP.call('GET', 'https://apis.google.com/js/platform.js');
-            return result;
-        } catch (err) {
-            // Got a network error, time-out or HTTP error in the 400 or 500 range.
-            return false;
-        }
-    }
-}});
-
-var Hangout = Meteor.call('hangout');
-
-
 // Hangout Methods
 
 Meteor.methods({
     renderSimpleHangout: function () {
-        console.log('render simple hangout');
-        console.log(Hangout);
+        console.log('render simple hangout')
+
+        //gapi.hangout.render('.hangout', {
+        //    'topic': 'hats',
+        //    'render': 'createhangout',
+        //    'initial_apps': [
+        //        { app_id : 'youtube' }
+        //    ]
+        //});
+
     }
 });

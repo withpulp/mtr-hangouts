6c17e7aa6632efdca6dc750b46ede729a8dd05a4
Husam Machlovi <husam@withpulp.com>
2015-09-13 17:03:43 -0400

    code to grab platform js data

--- app/packages/mtr-hangout/lib/server/hangout.js
+++ app/packages/mtr-hangout/lib/server/hangout.js
@@ -3,7 +3,23 @@
 Meteor.methods({
     renderSimpleHangout: function () {
         console.log('open hangout');
-
-
     }
 });
+
+//// API connection
+//if (Meteor.isServer) {
+//    Meteor.methods({
+//        hangoutsResult: function () {
+//            this.unblock();
+//            return Meteor.http.call("GET", "https://apis.google.com/js/platform.js");
+//        }
+//    });
+//}
+//
+////invoke the server method
+//if (Meteor.isClient) {
+//    Meteor.call("hangoutsResult", function(error, results) {
+//        console.log('this worked');
+//        console.log(results.content); //results.data should be a JSON object
+//    });
+//}

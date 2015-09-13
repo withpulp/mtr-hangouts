de36e7c5480ea842a3999adcbaeeeed2c9ba4b1f
Damir <damir@withpulp.com>
2015-09-13 16:42:47 -0400

    Hangout method returns API - callback is needed

--- app/packages/mtr-hangout/lib/server/hangout.js
+++ app/packages/mtr-hangout/lib/server/hangout.js
@@ -1,9 +1,11 @@
 // Hangout Methods
 
 Meteor.methods({
+    Hangout: function () {
+        this.unblock();
+        return Meteor.http.call('GET', 'https://apis.google.com/js/platform.js');
+    },
     renderSimpleHangout: function () {
         console.log('open hangout');
-
-
     }
 });
